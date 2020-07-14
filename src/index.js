df6b66233de8800990fa946dacf7bea813d170ae
MCauz <michaelhcausey@gmail.com>
2020-07-14 17:00:34 -0400

    main deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let newTask = document.getElementById(`create-task-form`)
+  const taskList = document.getElementById(`tasks`)
+  newTask.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    let task = document.getElementById(`new-task-description`)
+    // let taskText = task.value
+    taskList.innerHTML += `<li>${taskText}</li>`
+    document.getElementById(`new-task-description`).value = ""
+    // newTask.reset()
+  });
+
+
+    
 });
